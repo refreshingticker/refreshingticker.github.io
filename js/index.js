1b5e16474bd7a868d6137b5e4f4c47db10efc8f3
Refreshing Ticker <refreshingticker@gmail.com>
2017-03-14 16:39:16 -0400

    fixed cors error

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 	}
 
 	var numberOfRows = getURLParameter("rows");
-	$.getJSON("https://api.coinmarketcap.com/v1/ticker/?limit=" + numberOfRows).done(function(r){
+	/*$.getJSON("https://api.coinmarketcap.com/v1/ticker?limit=" + numberOfRows).done(function(r){
 		for(i=0;i<parseInt(numberOfRows);i++){
 			if(parseFloat(r[i]['percent_change_24h']) < 0){
 				$('#pricetable tr:last').after('<tr><td>' + r[i]['rank'] + '</td><td>' + '<a href="http://coinmarketcap.com/currencies/"' + r[i]['id'] + '"><img src="http://coinmarketcap.com/static/img/coins/16x16/' + r[i]['id'] + '.png" /></a>&nbsp;&nbsp;' + r[i]['name'] + '</td><td>$' + r[i]['price_usd'] + '</td><td>$' + parseFloat(r[i]['24h_volume_usd']).toLocaleString() + '</td><td class="red">' + parseFloat(r[i]['percent_change_24h']).toFixed(2) + '%</td></tr>');
@@ -13,5 +13,20 @@ $(document).ready(function() {
 				$('#pricetable tr:last').after('<tr><td>' + r[i]['rank'] + '</td><td><a href="http://coinmarketcap.com/currencies/' + r[i]['id'] + '"><img src="http://coinmarketcap.com/static/img/coins/16x16/' + r[i]['id'] + '.png" /></a>&nbsp;&nbsp;' + r[i]['name'] + '</td><td>$' + r[i]['price_usd'] + '</td><td>$' + parseFloat(r[i]['24h_volume_usd']).toLocaleString() + '</td><td class="green">' + parseFloat(r[i]['percent_change_24h']).toFixed(2) + '%</td></tr>');
 			}
 		}
+	});*/
+
+	$.ajax({
+	    crossOrigin: true,
+	    url: "https://api.coinmarketcap.com/v1/ticker/?limit=" + numberOfRows,
+	    success: function(raw) {
+	    var r = jQuery.parseJSON(raw);
+	    for(i=0;i<parseInt(numberOfRows);i++){
+			if(parseFloat(r[i]['percent_change_24h']) < 0){
+				$('#pricetable tr:last').after('<tr><td>' + r[i]['rank'] + '</td><td>' + '<a href="http://coinmarketcap.com/currencies/"' + r[i]['id'] + '"><img src="http://coinmarketcap.com/static/img/coins/16x16/' + r[i]['id'] + '.png" /></a>&nbsp;&nbsp;' + r[i]['name'] + '</td><td>$' + r[i]['price_usd'] + '</td><td>$' + parseFloat(r[i]['24h_volume_usd']).toLocaleString() + '</td><td class="red">' + parseFloat(r[i]['percent_change_24h']).toFixed(2) + '%</td></tr>');
+			}else{
+				$('#pricetable tr:last').after('<tr><td>' + r[i]['rank'] + '</td><td><a href="http://coinmarketcap.com/currencies/' + r[i]['id'] + '"><img src="http://coinmarketcap.com/static/img/coins/16x16/' + r[i]['id'] + '.png" /></a>&nbsp;&nbsp;' + r[i]['name'] + '</td><td>$' + r[i]['price_usd'] + '</td><td>$' + parseFloat(r[i]['24h_volume_usd']).toLocaleString() + '</td><td class="green">' + parseFloat(r[i]['percent_change_24h']).toFixed(2) + '%</td></tr>');
+			}
+		}
+	    }
 	});
 });
